db33b0eba9f32dd0412bc179261dadb61a56ae6d
travis holt <trvslhlt@gmail.com>
2026-05-30 16:32:48 -0400

    complete 03 guide

--- project_1/src/main/java/distributed/systems/LeaderElection.java
+++ project_1/src/main/java/distributed/systems/LeaderElection.java
@@ -12,17 +12,29 @@ public class LeaderElection implements Watcher {
     private static final int SESSION_TIMEOUT = 3000; // time before considering client dead
     private ZooKeeper zooKeeper;
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("running");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Running 'main'");
+        
         LeaderElection leaderElection = new LeaderElection();
         leaderElection.connectToZooKeeper();
+        leaderElection.run();
+        leaderElection.close();
 
-        System.out.println("Press Enter to exit the application...");
-        System.in.read(); // This blocks the main thread until you hit Enter
+        System.out.println("Disconnected from ZooKeeper. Exiting Applicaiton.");
+    }
+
+    public void run() throws InterruptedException {
+        synchronized (this.zooKeeper) {
+            this.zooKeeper.wait();
+        }
     }
 
     public void connectToZooKeeper() throws IOException{
         this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    }
+
+    public void close() throws InterruptedException {
+        this.zooKeeper.close();
     }
 
     // Watcher
@@ -32,6 +44,11 @@ public class LeaderElection implements Watcher {
             case None:
                 if (event.getState() == Event.KeeperState.SyncConnected) {
                     System.out.println("Successfully connected to ZooKeeper");
+                } else {
+                    synchronized (this.zooKeeper) {
+                        System.out.println("Disconnected from ZooKeeper event");
+                        this.zooKeeper.notifyAll();
+                    }
                 }
             default:
                 System.out.println("unhandled event");
